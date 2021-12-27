5e2c0eba51f10d233e827ea85753ea0f7c207778
Rakesh <iamrakeshkumar@pm.me>
2021-12-27 18:46:22 +0530

    style:sidebar

---
+++ src/Components/Sidebar/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Sidebar.css";
+
+export const SideBar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar-container">
+        <div className="brand">
+          <h3>Kyupid..</h3>
+        </div>
+
+        <div className="sidebar-menu">
+          <ul>
+            <li>
+              <span>Overview</span>
+            </li>
+            <li>
+              <span>Pro Users</span>
+            </li>
+            <li>
+              <span>Male female Ratio</span>
+            </li>
+            <li>
+              <span>Number of Users</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
