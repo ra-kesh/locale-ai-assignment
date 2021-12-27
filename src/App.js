b8b15ccf2ac21afaf754203d2bfec12ccb63d819
Rakesh <iamrakeshkumar@pm.me>
2021-12-27 18:47:31 +0530

    sidebar added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { SideBar } from "./Components";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <SideBar />
+    </div>
+  );
 }
 
 export default App;
