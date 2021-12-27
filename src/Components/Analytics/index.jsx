4e76069a7944e704544b2575c9695e886c8797d7
Rakesh <iamrakeshkumar@pm.me>
2021-12-28 01:32:24 +0530

    feat:analytics added

--- src/Components/Analytics/index.jsx
+++ src/Components/Analytics/index.jsx
@@ -1,21 +1,32 @@
 import React from "react";
+import { useUserData } from "../../Hooks/useUserData";
 import "./analytics.css";
 
 export const Analytics = () => {
+  const {
+    userData,
+    totalProUser,
+    proUserPercentage,
+    totalMaleUser,
+    totalFeMaleUser,
+    maleUserPercentage,
+    femaleUserPercentage,
+  } = useUserData();
   return (
     <section>
       <div className="analytics">
         <div className="analytic">
           <div className="analytic-info">
             <h4>Total users</h4>
-            <h1>10.3M</h1>
+            <h1>{userData.length}</h1>
           </div>
         </div>
         <div className="analytic">
           <div className="analytic-info">
             <h4>Total Pro users</h4>
             <h1>
-              20.9k <small className="text-danger">5%</small>
+              {totalProUser.length}{" "}
+              <small className="text-success">{proUserPercentage}%</small>
             </h1>
           </div>
         </div>
@@ -23,14 +34,18 @@ export const Analytics = () => {
           <div className="analytic-info">
             <h4>Total Male users</h4>
             <h1>
-              1.3k <small className="text-success">12%</small>
+              {totalMaleUser.length}{" "}
+              <small className="text-success">{maleUserPercentage}%</small>
             </h1>
           </div>
         </div>
         <div className="analytic">
           <div className="analytic-info">
             <h4>Total Female users</h4>
-            <h1>3.4M</h1>
+            <h1>
+              {totalFeMaleUser.length}{" "}
+              <small className="text-success">{femaleUserPercentage}%</small>
+            </h1>
           </div>
         </div>
       </div>
--- src/Hooks/useUserData.jsx
+++ src/Hooks/useUserData.jsx
@@ -3,7 +3,37 @@ import { UserContext } from "../Context/user-context";
 
 export const useUserData = () => {
   const { userData } = useContext(UserContext);
+
+  const totalProUser = userData?.filter(
+    ({ is_pro_user }) => is_pro_user === true
+  );
+
+  const proUserPercentage = (
+    (totalProUser.length / userData.length) *
+    100
+  ).toFixed();
+
+  const totalMaleUser = userData?.filter(({ gender }) => gender === "M");
+
+  const maleUserPercentage = (
+    (totalMaleUser.length / userData.length) *
+    100
+  ).toFixed();
+
+  const totalFeMaleUser = userData?.filter(({ gender }) => gender === "F");
+
+  const femaleUserPercentage = (
+    (totalFeMaleUser.length / userData.length) *
+    100
+  ).toFixed();
+
   return {
     userData,
+    totalProUser,
+    proUserPercentage,
+    totalMaleUser,
+    totalFeMaleUser,
+    maleUserPercentage,
+    femaleUserPercentage,
   };
 };
