8419df31943abc1a4c6cfa687ac3c9af2bea7c75
Rakesh <iamrakeshkumar@pm.me>
2021-12-27 19:05:06 +0530

    style:added header

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,15 @@
+import "./header.css";
+import React from "react";
+
+export const Header = () => {
+  return (
+    <header>
+      <div class="header-wrapper">
+        <div class="header-title">
+          <h1>Dashboard</h1>
+          <p>Displaying analytics for kyupid</p>
+        </div>
+      </div>
+    </header>
+  );
+};
